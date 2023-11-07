2429407a911b2337ff6c7e4e96f69a917a551b9d
Nadagani Amulya Prasanth <amulyaprasanth@users.noreply.huggingface.co>
2023-11-07 06:59:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ def predict_fn(
         raise CustomException(e, sys)
 
 description = """
-    <center><img title="car price prediction" alt="Car image" src="/images/cars.jpg"></center>
-    This project demonstrates how to use CatBoost to predict the price of used cars. The model was able to achieve an accuracy of over 90% on the test set. This suggests that CatBoost is a promising approach for car price prediction."""
+    <img title="car price prediction" alt="Car image" src="images/cars.jpg" align="center">
+    <center>This project demonstrates how to use CatBoost to predict the price of used cars. The model was able to achieve an accuracy of over 90% on the test set. This suggests that CatBoost is a promising approach for car price prediction.</center>"""
 demo = gr.Interface(fn=predict_fn,
                     inputs=input_list,
                     outputs=gr.Number(label="Selling Price of the Car", info="in lakhs"),
